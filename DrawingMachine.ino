211736d49de9dded5ab990e4adab0a0b0e975502
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2023-05-19 11:15:00 +0200

    Update DrawingMachine.ino

--- DrawingMachine.ino
+++ DrawingMachine.ino
@@ -2,38 +2,59 @@
 #include <math.h>
 
 const int stepsPerRevolution = 200;
-int posXdata = 500;
-int posYdata = 10;
-String mode = "NORMAL";
+
+volatile bool isStopAPressed = false;
+volatile bool isStopBPressed = false;
 
 Stepper StepperA = Stepper(stepsPerRevolution, 4,5,6,7);
 Stepper StepperB = Stepper(stepsPerRevolution, 8,9,10,11);
-
-float stringLenght = 630;//mm
+//what are the machine dimensions?
+float stringLength = 630;//mm
 float maxWidth = 740;//mm
-float maxHeight = -480;//mm
+float maxHeight = 480;//mm
+//data for serial
+int posXdata = maxWidth/2;//mm
+int posYdata = maxHeight;//mm
+String mode = "NORMAL";
 
-int currentPosRaw[2] = {0,0};
+//200 steps ~ 31mm
+//1 step = 0.155mm
+//1 mm = 6.45161290323 steps
+//converts step number to milimiters
+float stepsToMm(int steps){
+  return steps * 0.155;
+}
+int mmToSteps(float mm){
+  return mm * 6.45;
+}
+
+//position of the pencil
+int currentPosRaw[2] = {stringLength,stringLength};//steps
 //uppper left corner is [0,0]
-int currentPos[2] = {500,10};
+float currentPos[2] = {maxWidth/2,maxHeight};//mm
 
-void move(int posY, int posX){
-  int distancePenToStepper[2] = {
-    sqrt(pow(posX,2)+pow(posY,2)),
-    sqrt(pow(maxWidth - posX,2)+pow(posY,2))
+//moves the pen to specified location (in mm)
+void move(int X, int Y){
+  //convert from milimiters to the number of steps
+  int posX = mmToSteps(X);
+  int posY = mmToSteps(Y);
+
+  int distancePenToStepper[2] = {//in steps
+    sqrt(pow(posY,2)+pow(posX,2)),
+    sqrt(pow(mmToSteps(maxWidth) - posY,2)+pow(posX,2))
   };
 
   //calculate how much to move
-  int howMuchToMove[2] = {
+  int howMuchToMove[2] = {//in steps
     currentPosRaw[0] - distancePenToStepper[0],
     currentPosRaw[1] - distancePenToStepper[1]
   };
-/*
+  /*
   //calculate if the position is out of reach
-  if (currentPosRaw[0] + howMuchToMove[0] > stringLenght){
+  if (currentPosRaw[0] + howMuchToMove[0] > stringLength){
     howMuchToMove[0] = 0;
   }
-  if (currentPosRaw[1] + howMuchToMove[1] > stringLenght){
+  if (currentPosRaw[1] + howMuchToMove[1] > stringLength){
     howMuchToMove[1] = 0;
   }*/
   
@@ -54,34 +75,47 @@ void move(int posY, int posX){
     currentPosRaw[0] = currentPosRaw[0] - (currentPosRaw[0]-distancePenToStepper[0])/progres;
     currentPosRaw[1] = currentPosRaw[1] - (currentPosRaw[1]-distancePenToStepper[1])/progres;
   }
+  currentPos[0] = X;
+  currentPos[1] = Y;
 }
 
 void moveRaw(int posX, int posY){
   int numOfSteps = 100;
 
   //set the number of steps based on which distance is smaller
-  if(abs(currentPosRaw[0] - posX) < abs(currentPosRaw[1] - posY)){
-    numOfSteps = abs(currentPosRaw[0] - posX);
+  if(abs(currentPosRaw[0] - posY) < abs(currentPosRaw[1] - posX)){
+    numOfSteps = abs(currentPosRaw[0] - posY);
   }
   else{
-    numOfSteps = abs(currentPosRaw[1] - posY);
+    numOfSteps = abs(currentPosRaw[1] - posX);
   }
 
   //var progres is the number of steps until completion
   for(int progres=numOfSteps;progres>0;progres--){
-    StepperA.step((currentPosRaw[0]-posX)/progres);
-    StepperB.step((currentPosRaw[1]-posY)/progres);
-    currentPosRaw[0] = currentPosRaw[0] - (currentPosRaw[0]-posX)/progres;
-    currentPosRaw[1] = currentPosRaw[1] - (currentPosRaw[1]-posY)/progres;
+    StepperA.step((currentPosRaw[0]-posY)/progres);
+    StepperB.step((currentPosRaw[1]-posX)/progres);
+    currentPosRaw[0] = currentPosRaw[0] - (currentPosRaw[0]-posY)/progres;
+    currentPosRaw[1] = currentPosRaw[1] - (currentPosRaw[1]-posX)/progres;
   }
 }
 
-//run this if the stop button is pressed
-void stopA(){
-  //currentPosRaw[0] = 0;
-}
-void stopB(){
-  //currentPosRaw[1] = 0;
+void homeAll(){
+  //click the A button
+  while(isStopAPressed==false){
+    StepperA.step(-2);
+  }
+  isStopAPressed = false;
+  
+
+  //click the B button
+  while(isStopBPressed==false){
+    StepperB.step(2);
+  }
+  isStopBPressed = false;
+
+  //set coordinates
+  currentPos[0] = maxWidth/2;
+  currentPos[1] = maxHeight;
 }
 
 void setup() {
@@ -91,18 +125,31 @@ void setup() {
   StepperA.setSpeed(100);
   StepperB.setSpeed(100);
 
-  attachInterrupt(digitalPinToInterrupt(2), stopA, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), stopB, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), stopA, RISING);
+  attachInterrupt(digitalPinToInterrupt(2), stopB, RISING);
+}
+
+//run this if the stop button is pressed
+void stopA(){
+  isStopAPressed = true;
+  currentPosRaw[0] = mmToSteps(stringLength);
+}
+void stopB(){
+  isStopBPressed = true;
+  currentPosRaw[1] = mmToSteps(stringLength);
 }
 
 void loop() {
   if(Serial.available() > 0) {
-    //mode can be RAW or NORMAL
+    //mode can be RAW, NORMAL or HOME
     mode = (Serial.readStringUntil('\n'));
     posXdata = (Serial.readStringUntil('\n')).toInt();
     posYdata = (Serial.readStringUntil('\n')).toInt();
-    Serial.print(currentPosRaw[0]);
-    Serial.print(currentPosRaw[1]);
+    Serial.print("X: ");
+    Serial.print(currentPos[0]);
+    Serial.print("mm Y: ");
+    Serial.print(currentPos[1]);
+    Serial.print("mm");
   }
   if (mode=="NORMAL"){
     move(posXdata,posYdata);
@@ -111,6 +158,9 @@ void loop() {
   else if (mode=="RAW"){
     moveRaw(posXdata,posYdata);
     //Serial.println("OK");
+  }
+  else if (mode=="HOME"){
+    homeAll();
   }
   else{
     //Serial.println("ERROR");
