38aa593dcf29f35e48cfbba29d8efbcd520f7af9
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2023-08-23 17:18:20 +0200

    Update main.py

--- python_interface/main.py
+++ python_interface/main.py
@@ -4,11 +4,9 @@ import math
 import serial
 import time
 
-#TODO: fix weird vibrations when the gcode finishes
 #TODO: add a lookup feature (you see the
 # - estimated finish time
 # - where the cursor is
-# - the travel lines are in different collors if the pen is going to draw over them
 
 #serial comunications init
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -17,25 +15,38 @@ ser.reset_input_buffer()
 progresBarMax = 0;
 progresBar = 0;
 
-screenSizeX = 740#mm
-screenSizeY = 550#mm
-#position X, position Y, position Z (1-not drawing, 0-drawing)
-positions = [[740/2,510,1]]
-mouseX = 0
-mouseY = 0
+#what are the machine dimensions?
+#lenght of the string
+stringLength = 600;  #mm
+#how wide the machine is (the distance between 2 stepper motors)
+maxWidth = 736;  #mm
+#how low can the rope go
+maxHeight = math.sqrt(pow(stringLength, 2) - pow(maxWidth / 2, 2));
 
-#TODO put all of the machine dimensions as a variable that i can setup
+screenSizeX = maxWidth
+screenSizeY = maxHeight
+
 #where is the pencil at
-posX = 740/2
-posY = 510
+posX = screenSizeX/2
+posY = screenSizeY
+#posZ can be
+# 0 - dissablede
+# 1 - enabled
+# 2 - left motor can't reach
+# 3 - right motor can't reach
 posZ = 1
 mode = "NORMAL"
+
+#position X, position Y, position Z (1-not drawing, 0-drawing)
+positions = [[screenSizeX/2,screenSizeY,1]]
+mouseX = 0
+mouseY = 0
 
 filePath = "drawing.gcode"
 
 #tkinter init
 root = tk.Tk()
-root.title("robotic arm")
+root.title("CNC plotter")
 root.geometry("740x510")
 root.resizable(True,True)
 Bplace=tk.Canvas(root,width=740,height=40)
@@ -58,43 +69,79 @@ def openFile():
 	clearDrawing()
 
 	#read from gcode file
-	try:
-		file = open(filePath,"r")
-		#reads the file to get a visualisation on the screen
-		for line in file:
-			for word in line.split():
-				if word[0] == "(" or word[0] == ";":
-					break#skips this loop if there is a comment
+	if filePath.endswith(".gcode"):
+		try:
+			file = open(filePath,"r")
+			#reads the file to get a visualisation on the screen
+			for line in file:
+				for word in line.split():
+					if word[0] == "(" or word[0] == ";":
+						break#skips this loop if there is a comment
 
-				if word == "G1" or word == "G0":
-					pass
-
-				if word[0] == "X":
-					cleared_string = word.replace("X", "")
-					try:
-						posX = float(cleared_string)
-					except ValueError:
+					if word == "G1" or word == "G0":
 						pass
-				if word[0] == "Y":
-					cleared_string = word.replace("Y", "")
-					try:
-						posY = float(cleared_string)
-					except ValueError:
-						pass
-				if word[0] == "Z":
-					try:
-						cleared_string = word.replace("Z", "")
-						posZ = int(cleared_string)
-					except ValueError:
-						pass
-			#at the end of the line push to the position array the positions
-			positions.append([posX,posY,posZ])
-			progresBarMax += 1
-		file.close()
 
-	except TypeError:
-		print("ERROR: can't open file")
+					if word[0] == "X":
+						cleared_string = word.replace("X", "")
+						try:
+							posX = float(cleared_string)
+						except ValueError:
+							pass
+					if word[0] == "Y":
+						cleared_string = word.replace("Y", "")
+						try:
+							posY = float(cleared_string)
+						except ValueError:
+							pass
+					if word[0] == "Z":
+						try:
+							cleared_string = word.replace("Z", "")
+							posZ = int(cleared_string)
+						except ValueError:
+							pass
 
+				#check if one of the lines is too short to reach the point
+				if (math.sqrt(pow(posX, 2) + pow(posY, 2)) > stringLength):
+					positions.append([posX,posY,2])
+				elif (math.sqrt(pow(maxWidth - posX, 2) + pow(posY, 2)) > stringLength):
+					positions.append([posX,posY,3])
+				else:
+					positions.append([posX,posY,posZ])
+
+				progresBarMax += 1
+			file.close()
+
+		except TypeError:
+			print("ERROR: can't open file")
+	elif filePath.endswith(".svg"):
+		print ("svg!!!")
+	else:
+		print ("ERROR: this file format is not supported")
+
+def drawTheScreen():
+	global posX
+	global posY
+	global screenSizeX
+	global screenSIzeY
+	global positions
+
+	#clear the screen
+	place.delete("all")
+	#draw
+	#background
+	place.create_rectangle(0,0,root.winfo_width(),root.winfo_height(),fill='#101010')
+	#debug cube
+	place.create_rectangle((posX/screenSizeX*root.winfo_width())-5,(posY/screenSizeY*root.winfo_height())-5,(posX/screenSizeX*root.winfo_width())+5,(posY/screenSizeY*root.winfo_height())+5,fill='#fb1b1b')
+	#draw lines on screen
+	for i in range(len(positions)-1):
+		if positions[i][2] == 1:
+			place.create_line(positions[i][0]/screenSizeX*root.winfo_width(),positions[i][1]/screenSizeY*root.winfo_height(),positions[i+1][0]/screenSizeX*root.winfo_width(),positions[i+1][1]/screenSizeY*root.winfo_height(),fill="#A0A040",width=1)
+		elif positions[i][2] == 2:
+			place.create_line(positions[i][0]/screenSizeX*root.winfo_width(),positions[i][1]/screenSizeY*root.winfo_height(),positions[i+1][0]/screenSizeX*root.winfo_width(),positions[i+1][1]/screenSizeY*root.winfo_height(),fill="#ff0000",width=1)
+		elif positions[i][2] == 3:
+			place.create_line(positions[i][0]/screenSizeX*root.winfo_width(),positions[i][1]/screenSizeY*root.winfo_height(),positions[i+1][0]/screenSizeX*root.winfo_width(),positions[i+1][1]/screenSizeY*root.winfo_height(),fill="#ff0000",width=1)
+		else:
+			place.create_line(positions[i][0]/screenSizeX*root.winfo_width(),positions[i][1]/screenSizeY*root.winfo_height(),positions[i+1][0]/screenSizeX*root.winfo_width(),positions[i+1][1]/screenSizeY*root.winfo_height(),fill="#f0f0f0",width=1)
 
 def sendSerialData():
 	global posX
@@ -140,11 +187,11 @@ def gotoFunction():
 
 def clearDrawing():
 	global positions
-	positions = [[740/2,510,1]]
+	positions = [[screenSizeX/2,screenSizeY,1]]
 
 #run all the possitions until there are none left
 def run():
-	global mode
+	global modefile
 	global posX
 	global posY
 	global posZ
@@ -190,6 +237,9 @@ def run():
 		sendSerialData()
 		serialInput = ser.readline().decode('utf-8').rstrip()
 
+		#while the program waits for input it redraws the screen
+		#drawTheScreen()
+
 		while serialInput == "":
 			print("waiting for input")
 			serialInput = ser.readline().decode('utf-8').rstrip()
@@ -232,20 +282,7 @@ openFileButton = tk.Button(text='open file',command=openFile)
 Bplace.create_window(600,20,window=openFileButton)
 
 def main():
-	#clear the screen
-	place.delete("all")
-	#draw
-	#background
-	place.create_rectangle(0,0,root.winfo_width(),root.winfo_height(),fill='#101010')
-	#debug cube
-	place.create_rectangle((posX/screenSizeX*root.winfo_width())-5,(posY/screenSizeY*root.winfo_height())-5,(posX/screenSizeX*root.winfo_width())+5,(posY/screenSizeY*root.winfo_height())+5,fill='#fb1b1b')
-	#draw lines on screen
-	for i in range(len(positions)-1):
-		if positions[i][2] == 1:
-			place.create_line(positions[i][0]/screenSizeX*root.winfo_width(),positions[i][1]/screenSizeY*root.winfo_height(),positions[i+1][0]/screenSizeX*root.winfo_width(),positions[i+1][1]/screenSizeY*root.winfo_height(),fill="#A0A040",width=1)
-		else:
-			place.create_line(positions[i][0]/screenSizeX*root.winfo_width(),positions[i][1]/screenSizeY*root.winfo_height(),positions[i+1][0]/screenSizeX*root.winfo_width(),positions[i+1][1]/screenSizeY*root.winfo_height(),fill="#f0f0f0",width=1)
-
+	drawTheScreen()
 	root.after(2,main)
 
 #place.bind('<Motion>',motion)
